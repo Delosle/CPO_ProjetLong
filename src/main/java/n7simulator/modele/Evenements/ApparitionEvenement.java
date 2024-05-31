9b97f036c2be1a362970813d456f2b54a3b628ef
mathis <mathisbanzet@orange.fr>
2024-05-31 21:39:21 +0200

    modif description evenement irregulier

--- src/main/java/n7simulator/modele/Evenements/ApparitionEvenement.java
+++ src/main/java/n7simulator/modele/Evenements/ApparitionEvenement.java
@@ -1,4 +1,20 @@
 package n7simulator.modele.Evenements;
 
+import java.sql.Connection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import n7simulator.database.DatabaseConnection;
+
 public class ApparitionEvenement {
+
+    private
+
+    public ApparitionEvenement() {
+
+    }
+
+    private List< LinkedHashMap <String, Object>> recupDonneeEvenement(){
+        Connection conn = DatabaseConnection.getDBConnexion();
+        int nbEvenement = DatabaseConnection.effectuerRequete("SELECT MAX(idPartie) as maxId FROM Partie;", conn).getInt("maxId");
+    }
 }
