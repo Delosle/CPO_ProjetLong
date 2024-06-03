7bc0b58f902b54f0722ce0fa4d0fcc37f3f97f7f
GalateeM <galatee.m@gmail.com>
2024-06-03 18:06:34 +0200

    modifs sql

--- src/main/resources/Creer_Tables_Sauvegarde.sql
+++ src/main/resources/Creer_Tables_Sauvegarde.sql
@@ -1,8 +1,9 @@
 CREATE TABLE ProfEmbauches (
-    idprof INTEGER PRIMARY KEY,
+    idprof INTEGER,
     salaire INTEGER NOT NULL,
     nbheure INTEGER NOT NULL,
-    idPartie INTEGER NOT NULL  REFERENCES Partie(idPartie)
+    idPartie INTEGER NOT NULL  REFERENCES Partie(idPartie),
+    PRIMARY KEY (idprof, idPartie)
 );
 
 CREATE TABLE EvenementEnCours (
@@ -15,11 +16,11 @@ CREATE TABLE Partie (
     idPartie INTEGER PRIMARY KEY,
     nomPartie TEXT NOT NULL,
     estPerdue BOOLEAN NOT NULL,
-    nbJours INT NOT NULL,
+    dateEnCours DATE NOT NULL,
     nbEleves INT NOT NULL,
     argent FLOAT NOT NULL,
-    bonheur INT NOT NULL,
-    pedagogie INT NOT NULL,
+    bonheur FLOAT NOT NULL,
+    pedagogie FLOAT NOT NULL,
     idQualiteRepasCrous INT NOT NULL,
     prixVenteRepascrous FLOAT NOT NULL
 );
