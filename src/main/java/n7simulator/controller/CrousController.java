1c4e4cf982e4958c87998e1368175d4897c0ecf3
DamienLanusse <damien.lanusse@etu.inp-n7.fr>
2024-06-03 09:57:24 +0200

    Controller et Vue fonctionnels (je crois)

--- src/main/java/n7simulator/controller/CrousController.java
+++ src/main/java/n7simulator/controller/CrousController.java
@@ -7,7 +7,9 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -19,6 +21,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import n7simulator.modele.Partie;
 import n7simulator.vue.N7Frame;
@@ -33,50 +37,87 @@ public class CrousController extends JPanel {
 	private JSpinner spinnerPrixRevente;
 	
 	private JLabel marge;
-
+	
 	public CrousController() {
 		// TODO : Récupérer l'instance du modèle de Crous
 		String[] options = {"Mauvaise : 1.10€", "Acceptable : 1.30€", "Bonne : 1.70€", "Très bonne : 2.00€"};
 		
+		// Création du contenant qui va contenir les éléments du controller
 		JPanel contenant = new JPanel(new GridLayout(3,1));
 		
+		// Le contenant qui va contenir les éléments de la qualité
 		JPanel qualite = new JPanel();
 		qualite.setLayout(new BoxLayout(qualite, BoxLayout.Y_AXIS));
+		
+		// On créé une boîte pour que le texte soit placé à gauche
 		Box b = Box.createHorizontalBox();
 		b.add(new JLabel("Qualité :"));
 	    b.add( Box.createHorizontalGlue() );
-		qualite.add(b);
+		// On créé le sélecteur
 		selecteur = new JComboBox(options);
-		// TODO : placer la valeur de la qualité
+		selecteur.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				updateMarge();
+			}
+		});
+		
+		// On place les éléments
+		qualite.add(b);
 		qualite.add(selecteur);
+		qualite.setBorder(BorderFactory.createEmptyBorder(0, 0, 15, 0));
 		contenant.add(qualite);
 		
+		// Le contenant qui va contenir les éléments de prix de revente et marge
 		JPanel prixRevente = new JPanel(new GridLayout(1,2));
 		
+		// Le contenant qui contient les éléments de prix de revente
 		JPanel revente = new JPanel();
 		revente.setLayout(new BoxLayout(revente, BoxLayout.Y_AXIS));
+		
+		// On créé une boîte pour que le texte soit placé à gauche
 		Box bPR = Box.createHorizontalBox();
-		bPR.add(new JLabel("Prix Revente :"));
+		bPR.add(new JLabel("Prix de revente :"));
 	    bPR.add( Box.createHorizontalGlue() );
-		revente.add(bPR);
-		// TODO : placer la valeur du prix de revente
+		
+	    // On créé la zone de saisie du prix
 		spinnerPrixRevente = new JSpinner(
-				new SpinnerNumberModel(0.0, 0.0, 100.0, 0.1));
+				new SpinnerNumberModel(0.0, 0.0, 100.0, 0.01));
+		spinnerPrixRevente.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				updateMarge();
+			}
+		});
+		
+		// On place les éléments
+		revente.add(bPR);
 		revente.add(spinnerPrixRevente);
 		prixRevente.add(revente);
 		
+		// On créé le contenant de la marge
 		JPanel laMarge = new JPanel();
 		laMarge.setLayout(new BoxLayout(laMarge, BoxLayout.Y_AXIS));
-		laMarge.add(new JLabel("Marge :"));
+		JLabel titleMarge = new JLabel("Marge :");
+		titleMarge.setAlignmentX(CENTER_ALIGNMENT);
 		
-		// TODO : récupérer la valeur de la marge
-		marge = new JLabel("+ 2 €");
+		// On créé la zone de la marge qui va se modifier régulièrement
+		marge = new JLabel();
+		marge.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+
+		// On place les éléments
+		laMarge.add(titleMarge);
 		laMarge.add(marge);
-		prixRevente.add(laMarge);
-		
+		prixRevente.add(laMarge, BorderLayout.CENTER);
 		contenant.add(prixRevente);
 		
-		// TODO : réaliser le calcul de la marge
+		// On modifie les valeurs
+		// TODO : placer la valeur de la qualité
+		selecteur.setSelectedIndex(2);
+		// TODO : placer la valeur du prix de revente
+		spinnerPrixRevente.setValue(1.05);
+		
 		
 		// Afficher la boite de dialogue
 		int result = JOptionPane.showConfirmDialog(null, contenant, "Gestion du contrat",
@@ -89,12 +130,31 @@ public class CrousController extends JPanel {
 		
 	}
 	
+	private void updateMarge() {
+		// TODO récupérer la liste des prix autrement svp
+		int indexQualite = selecteur.getSelectedIndex();
+		double[] listePrix = {1.10, 1.30, 1.70, 2.00};
+		Double prixRevente = (Double)spinnerPrixRevente.getValue();
+		int nbEleves = Partie.getInstance().getNombreEleves();
+		Double valeur = nbEleves * (prixRevente - listePrix[indexQualite]);
+		String text = "";
+		if (valeur > 0) {
+			text += "+ ";
+		}
+		else {
+			text += "- ";
+			valeur = valeur * -1.0;
+		}
+		text += "%.2f".formatted(valeur);
+		text += " €";
+		marge.setText(text);
+	}
+	
 	public static JButton getBoutonOuverture() {
 		JButton leBouton = new JButton("Modifier");
 		ActionListener ouvrirModification = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CrousController fenetre = new CrousController();
-				//N7Frame.getInstance(null, null).ajouterLayer(fenetre, JLayeredPane.MODAL_LAYER);
 			}
 		};
 		leBouton.addActionListener(ouvrirModification);
--- src/main/java/n7simulator/vue/CrousGUI.java
+++ src/main/java/n7simulator/vue/CrousGUI.java
@@ -13,6 +13,8 @@ import java.awt.Toolkit;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -39,29 +41,42 @@ public class CrousGUI extends JPanel implements Observer {
 		qualite = 1;
 		prixRevente = 2.0;
 
-		this.setLayout(new GridLayout(2,1));
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
 		
-
-		this.add(new JLabel("CROUS : "));
+		// Zone titre
+		Box title = Box.createHorizontalBox();
+		title.add(new JLabel("CROUS : "));
+		title.add(Box.createHorizontalGlue());
+		title.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+		this.add(title);
 		
+		// Zone contenu
 		JPanel contenu = new JPanel(new GridLayout(1,2));
 		
-		JPanel informations = new JPanel(new GridLayout(2,1));
+		// Zone informations
+		JPanel informations = new JPanel();
+		informations.setLayout(new BoxLayout(informations, BoxLayout.Y_AXIS));
 		
-		JPanel qualiteRepas = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		// Ligne qualité repas
+		Box qualiteRepas = Box.createHorizontalBox();
 		qualiteRepas.add(new JLabel("Qualité du repas : "));
 		labelQualite = new JLabel(qualite.toString());
 		qualiteRepas.add(labelQualite);
+		qualiteRepas.add(Box.createHorizontalGlue());
 		informations.add(qualiteRepas);
 		
-		JPanel panelRevente = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		// Ligne prix de revente
+		Box panelRevente = Box.createHorizontalBox();
 		panelRevente.add(new JLabel("Prix de revente du repas : "));
 		labelRevente = new JLabel(prixRevente.toString());
 		panelRevente.add(labelRevente);
+		panelRevente.add(Box.createHorizontalGlue());
 		informations.add(panelRevente);
 		
 		contenu.add(informations, BorderLayout.WEST);
 		
+		// Zone bouton modification
 		contenu.add(CrousController.getBoutonOuverture(), BorderLayout.EAST);
 
 		this.add(contenu);
@@ -70,7 +85,7 @@ public class CrousGUI extends JPanel implements Observer {
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		// TODO Modifier les labels
 		
 	}
 	
