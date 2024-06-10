cb348173d7bd828d65514a34e794fb8efc416ff7
LeilaMartinet <leila.mrtnt1@gmail.com>
2024-06-10 16:28:35 +0200

    ajout test impact jour suivant eleves

--- src/test/java/tests/GestionElevesTest.java
+++ src/test/java/tests/GestionElevesTest.java
@@ -2,11 +2,23 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.lang.reflect.Field;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import n7simulator.modele.GestionEleves;
+import n7simulator.modele.Partie;
+import n7simulator.modele.jauges.Jauge;
 
 public class GestionElevesTest {
+	
+	@Before
+	public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+	   Field instance = Partie.class.getDeclaredField("instance");
+	   instance.setAccessible(true);
+	   instance.set(null, null);
+	}
 	
 	/*
 	 * Test vérifiant que les élèves inscrits sont bien ajoutés.
@@ -21,7 +33,7 @@ public class GestionElevesTest {
 	}
 	
 	/*
-	 * Test vérifiant que les élèves désinscrits sont bien ajoutés.
+	 * Test vérifiant que les élèves désinscrits sont bien supprimés.
 	 */
 	@Test
 	public void testDesinscrireEleves() {
@@ -44,5 +56,36 @@ public class GestionElevesTest {
 		gestElv.desinscrireEleves(oldNbEleves);
 		gestElv.desinscrireEleves(nbExEleves);
 		assertEquals(0, gestElv.getNombreEleves());		
+	}
+	
+	/*
+	 * Test vérifiant que les élèves désinscrits sont bien ajoutés.
+	 */
+	@Test
+	public void testImpactJourSuivant() {
+		Partie partie = Partie.getInstance();
+		Jauge bonheur = partie.getJaugeBonheur();
+		Jauge pedagogie = partie.getJaugePedagogie();
+		GestionEleves gstElv = partie.getGestionEleves();
+		int niveauBohneur = 92;
+		int niveauPedagogie = 34;
+		int nbEleves = 98;
+		
+		// Remise à zéro des jauges
+		bonheur.ajouter(-(bonheur.getValue()));
+		pedagogie.ajouter(-(pedagogie.getValue()));
+		gstElv.desinscrireEleves(gstElv.getNombreEleves());
+		
+		bonheur.ajouter(niveauBohneur);
+		pedagogie.ajouter(niveauPedagogie);		
+		partie.getGestionEleves().inscrireEleves(nbEleves);
+		
+		partie.getGestionEleves().effectuerImpactJourSuivant();
+		
+		int gainMax = (nbEleves / 5) + 10;
+		int totalJauges = (int)(partie.getJaugeBonheur().getValue() + partie.getJaugePedagogie().getValue()) / 2;
+		int gain = totalJauges * gainMax / 100;
+		
+		assertEquals(gain + nbEleves, partie.getGestionEleves().getNombreEleves());
 	}
 }
